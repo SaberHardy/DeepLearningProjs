8f0548794569423ffc0a627dfa4f5f04bc69ee46
Consultant <samirsamado5@gmail.com>
2024-02-20 20:16:50 -0500

    compile the model

--- Starter/BatchNorm.py
+++ Starter/BatchNorm.py
@@ -6,17 +6,17 @@ from tensorflow.keras import layers
 
 red_wine = pd.read_csv('../datasets/winequality-red.csv')
 
+print(red_wine.head())
 # Create training and validation split
 df_train = red_wine.sample(frac=0.7, random_state=0)
 df_valid = red_wine.drop(df_train.index)
 
+# Split features and target
 x_train = df_train.drop('quality', axis=1)
-x_valid = df_train.drop('quality', axis=1)
-
+x_valid = df_valid.drop('quality', axis=1)
 y_train = df_train['quality']
 y_valid = df_valid['quality']
 
-print(df_train)
 model = keras.Sequential([
     layers.Dense(1024, activation='relu', input_shape=[11]),
     layers.Dropout(0.3),
@@ -29,3 +29,20 @@ model = keras.Sequential([
     layers.BatchNormalization(),
     layers.Dense(1),
 ])
+# fixed acidity;"volatile acidity";"citric acid";"residual sugar";"chlorides";
+# "free sulfur dioxide";"total sulfur dioxide";"density";"pH";"sulphates";"alcohol";"quality"
+model.compile(
+    optimizer='adam',
+    loss='mae',
+)
+
+history = model.fit(
+    x_train, y_train,
+    validation_data=(x_valid, y_valid),
+    batch_size=256,
+    epochs=100,
+    verbose=0,
+)
+
+history_df = pd.DataFrame(history.history)
+history_df.loc[:, ['loss', 'val_loss']].plot()
